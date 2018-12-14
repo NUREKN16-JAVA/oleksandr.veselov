c2211b700648de588866028eb288bd085f887f14
Alexander <32273263+paniker228@users.noreply.github.com>
2018-12-14 17:46:04 +0200

    Upload lab5

--- usermanagement/src/test/java/ua/nure/cs/veselov/usermanagement/db/MockUserDao.java
+++ usermanagement/src/test/java/ua/nure/cs/veselov/usermanagement/db/MockUserDao.java
@@ -48,4 +48,9 @@ public class MockUserDao implements UserDAO {
 
     }
 
+    @Override
+    public Collection find(String firstName, String lastName) throws DatabaseException {
+        throw new UnsupportedOperationException();
+    }
+
 }
